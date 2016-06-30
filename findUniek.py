0fad8348600e092d179ca861683596505da5b6a6
Marliess <liezq@live.nl>
2016-06-30 14:41:59 +0200

    Update findUniek.py

--- findUniek.py
+++ findUniek.py
@@ -1,17 +1,14 @@
 def findUniek():
-    f = open("femaletags.txt","r")
-    f_match = open("maletags.txt","r")
+    file = open("femaletags.txt","r")
+    file_match = open("maletags.txt","r")
     dictio = []
-    for line in f:
+    for line in file:
         a, b = line.split()
         dictio.append(b)
 
-    for line in f_match:
+    for line in file_match:
         if line.split()[1] not in dictio:
             print(line)
         
-
-
-
 if __name__ == "__main__":
     findUniek()
